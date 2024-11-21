a0477274a3c29ea347cef79261011d0a1bb896f9
MRX <sirhb77@gmail.com>
2024-11-21 18:39:14 +0100

    feature final price

--- src/Components/CarValue.js
+++ src/Components/CarValue.js
@@ -1,4 +1,19 @@
+import { useSelector } from "react-redux";
+
 const CarValue =()=>{
-    return <div>CarValue</div>
+    const totalCost = useSelector(({cars:{data,searchTerm}})=>{
+        const filterdCars= data.filter((car)=>{
+            return car.name.toLowerCase().includes(searchTerm.toLowerCase())
+        })
+        let cost = 0 
+        for (let car of filterdCars){
+            cost += car.cost
+        }
+        return cost
+    })
+
+    return <div>
+        Total cost is:${totalCost}
+    </div>
 }
 export default CarValue;
