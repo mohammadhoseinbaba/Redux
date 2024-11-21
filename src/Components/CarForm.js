081cb6de805903ab2bd896a48275a51d8aaf853c
MRX <sirhb77@gmail.com>
2024-11-21 12:42:07 +0100

    updated

--- src/Components/CarForm.js
+++ src/Components/CarForm.js
@@ -1,8 +1,8 @@
 import { useDispatch, useSelector } from "react-redux"
-import { changeName } from "../Store"
+import { addCar, changeCost, changeName } from "../Store"
 
 const CarForm = () => {
-        
+
 
     const dispatch = useDispatch()
 
@@ -15,16 +15,36 @@ const CarForm = () => {
         dispatch(changeName(event.target.value))
     }
 
+    const handleCostChange = (event) => {
+        const carCost = parseInt(event.target.value) || 0
+        dispatch(changeCost(carCost))
+    }
 
+    const cost = useSelector((state) => {
+        return state.form.cost
+    })
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        dispatch(addCar({name,cost}))
+
+    }
 
     return <div className="car-form panle">
         <h4 className="subtitle is-3">Add Car</h4>
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className="field-group">
                 <div className='field'>
                     <label className="lable">Name</label>
-                    <input className="input is-expanded" value={name} onChange={handleNameChange}/>
+                    <input className="input is-expanded" value={name} onChange={handleNameChange} />
                 </div>
+                <div className='field'>
+                    <label className="lable">cost</label>
+                    <input className="input is-expanded" value={cost || ""} type="number" onChange={handleCostChange} />
+                </div>
+            </div>
+            <div>
+                <button>Submit</button>
             </div>
         </form>
     </div>
--- src/Components/CarList.js
+++ src/Components/CarList.js
@@ -1,4 +1,26 @@
-const CarList=()=>{
-    return <div>CarList</div>
+import { useDispatch, useSelector } from "react-redux"
+import { removeCar } from "../Store"
+
+
+const CarList = () => {
+
+    const dispatch = useDispatch()
+
+    const cars = useSelector((state) => {
+        return state.cars.data
+    })
+    const handleCarDelete = (car) => {
+        dispatch(removeCar(car.id))
+    }
+
+    const renderedcars = cars.map((car) => {
+        return <div key={car.id} className="panel ">
+            <p>
+                {car.name}-${car.cost}
+            </p>
+            <button className="button is-danger" onClick={() => { handleCarDelete(car) }}>Delete</button>
+        </div>
+    })
+    return <div>{renderedcars}<hr /></div>
 }
 export default CarList
